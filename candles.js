28f5f93c185e5f31568f51d4f9445bd1753b7c09
GingerNinja <louis23412@gmail.com>
2022-02-05 19:14:04 +0200

    use UTC+0 for timestamps

--- candles.js
+++ candles.js
@@ -23,33 +23,26 @@ const saveCandles = () => {
 const createCandle = (startTime, endTime) => {
     globalState.candleCounter++;
     globalState.candleDataBase.push({
-        "latest" : {
-            candleNum : globalState.candleCounter,
-            start : startTime,
-            endTime : endTime,
-            candleTimeSize : (endTime - startTime) / 1000,
+        start : new Date(startTime - Math.abs(new Date().getTimezoneOffset())).toISOString(),
+        end : new Date(endTime - Math.abs(new Date().getTimezoneOffset())).toISOString(),
+        candleNum : globalState.candleCounter,
+        candleSecs : (endTime - startTime) / 1000,
+
+        latest : {
             open : globalState.tempPriceHolder1[0],
             high : Math.max(...globalState.tempPriceHolder1),
             close : globalState.tempPriceHolder1[globalState.tempPriceHolder1.length -1],
             low : Math.min(...globalState.tempPriceHolder1)
         },
 
-        "lowest_ask" : {
-            candleNum : globalState.candleCounter,
-            start : startTime,
-            endTime : endTime,
-            candleTimeSize : (endTime - startTime) / 1000,
+        lowestAsk : {
             open : globalState.tempPriceHolder2[0],
             high : Math.max(...globalState.tempPriceHolder2),
             close : globalState.tempPriceHolder2[globalState.tempPriceHolder2.length -1],
             low : Math.min(...globalState.tempPriceHolder2)
         },
 
-        "highest_bid" : {
-            candleNum : globalState.candleCounter,
-            start : startTime,
-            endTime : endTime,
-            candleTimeSize : (endTime - startTime) / 1000,
+        highestBid : {
             open : globalState.tempPriceHolder3[0],
             high : Math.max(...globalState.tempPriceHolder3),
             close : globalState.tempPriceHolder3[globalState.tempPriceHolder3.length -1],
@@ -73,7 +66,8 @@ const createCandle = (startTime, endTime) => {
 }
 
 const broadcastCandle = () => {
-    const json = JSON.stringify(globalState.candleDataBase.slice(-1));
+    const json = JSON.stringify(globalState.candleDataBase.slice(-1)[0]);
+
     try {
         hive.broadcast.customJson(bKey, [], [bUser], `${bUser}-candleMaker`, json, function(err, result) {
             if (err) {
